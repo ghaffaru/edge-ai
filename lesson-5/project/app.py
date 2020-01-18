f3e86e7926b073e7323fa41237be58f7c98deab9
Ghaffaru <mudashiruagm@gmail.com>
2020-01-18 06:41:30 +0000

    set up node server

--- lesson-5/project/app.py
+++ lesson-5/project/app.py
@@ -8,3 +8,58 @@ import paho.mqtt.client as mqtt
 from random import randint
 from inference import Network
 
+INPUT_STREAM = "test_video.mp4"
+CPU_EXTENSION = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
+ADAS_MODEL = "/home/workspace/models/semantic-segmentation-adas-0001.xml"
+
+CLASSES = ['road', 'sidewalk', 'building', 'wall', 'fence', 'pole',
+'traffic_light', 'traffic_sign', 'vegetation', 'terrain', 'sky', 'person',
+'rider', 'car', 'truck', 'bus', 'train', 'motorcycle', 'bicycle', 'ego-vehicle']
+
+# MQTT server environment variables
+HOSTNAME = socket.gethostname()
+IPADDRESS = socket.gethostbyname(HOSTNAME)
+MQTT_HOST = IPADDRESS
+MQTT_PORT = None ### TODO: Set the Port for MQTT
+MQTT_KEEPALIVE_INTERVAL = 60
+
+def get_args():
+    '''
+    Gets the arguments from the command line.
+    '''
+    parser = argparse.ArgumentParser("Run inference on an input video")
+    # -- Create the descriptions for the commands
+    i_desc = "The location of the input file"
+    d_desc = "The device name, if not 'CPU'"
+
+    # -- Create the arguments
+    parser.add_argument("-i", help=i_desc, default=INPUT_STREAM)
+    parser.add_argument("-d", help=d_desc, default='CPU')
+    args = parser.parse_args()
+
+    return args
+
+
+def draw_masks(result, width, height):
+    '''
+    Draw semantic mask classes onto the frame.
+    '''
+    # Create a mask with color by class
+    classes = cv2.resize(result[0].transpose((1, 2, 0)), (width, height),
+                         interpolation=cv2.INTER_NEAREST)
+    unique_classes = np.unique(classes)
+    out_mask = classes * (255 / 20)
+
+    # Stack the mask so FFmpeg understands it
+    out_mask = np.dstack((out_mask, out_mask, out_mask))
+    out_mask = np.uint8(out_mask)
+
+    return out_mask, unique_classes
+
+def get_class_names(class_nums):
+    class_names= []
+    for i in class_nums:
+        class_names.append(CLASSES[int(i)])
+    return class_names
+
+
